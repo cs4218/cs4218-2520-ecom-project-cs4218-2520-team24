5c72a109c1bf1310261e4621a53778a5ef151679
Choo Jia Rong <122244985+jrchoo@users.noreply.github.com>
2026-02-21 18:34:32 +0800

    Update jest.backend.config.js

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -6,7 +6,11 @@ export default {
   testEnvironment: "node",
 
   // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js"],
+  testMatch: [
+    "<rootDir>/controllers/*.test.js",
+    "<rootDir>/helpers/*.test.js",
+    "<rootDir>/middlewares/*.test.js",
+  ],
 
   // jest code coverage
   collectCoverage: true,
